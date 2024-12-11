4d8391d41cbc49541803b0fdf0b06d3936d58116
Canaan Zhu <jianaz11@uci.edu>
2024-12-10 17:56:08 -0800

    Fixed up some ui stuff, and added a title screen and a item pickup event

--- TheGameProject/Assets/Scripts/Player_Interaction.cs
+++ TheGameProject/Assets/Scripts/Player_Interaction.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 public class Player_Interaction : MonoBehaviour
 {
     [SerializeField] private LayerMask NPC;
+    [SerializeField] private LayerMask items;
     [SerializeField] private float interactRange = 1f;
+    public string heldItem = "none";
     public bool interacting = false;
     private string currentNPC;
 
@@ -14,6 +16,7 @@ public class Player_Interaction : MonoBehaviour
     void Update()
     {
         Collider2D[] interactableNPC = Physics2D.OverlapCircleAll(transform.position, interactRange, NPC);
+        Collider2D[] interactableItems = Physics2D.OverlapCircleAll(transform.position, interactRange, items);
         if (interacting && Input.GetKeyDown(KeyCode.Space))
         {
             foreach (Collider2D my_NPC in interactableNPC)
@@ -25,11 +28,17 @@ public class Player_Interaction : MonoBehaviour
         {
             foreach (Collider2D my_NPC in interactableNPC)
             {
-
                 my_NPC.GetComponent<NPC_Dialogue_Trigger>().TriggerDialogue();
                 currentNPC = my_NPC.name;
                 interacting = true;
             }
+            foreach (Collider2D item in interactableItems)
+            {
+                item.GetComponent<Item_Pickup>().EventTriggered();
+                heldItem = item.name;
+                Debug.Log(heldItem);
+            }
+            
         }
         else
         {
