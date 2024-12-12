c8a5bbb16bbf7ea03d60dde870046634011a422a
WilliamChenGit2023 <129251589+WilliamChenGit2023@users.noreply.github.com>
2024-12-11 23:51:21 -0800

    fun2

--- TheGameProject/Assets/Scripts/RotateToTarget.cs
+++ TheGameProject/Assets/Scripts/RotateToTarget.cs
@@ -7,13 +7,15 @@ public class RotateToTarget : MonoBehaviour
 {
     [SerializeField] private float rotationSpeed;
     [SerializeField] private float moveSpeed;
+    [SerializeField] private Transform p;
     private Vector2 dirc;
+    public Vector3 y2 =  new Vector3(0,-0.5f,0);
 
 
     // Update is called once per frame
     void Update()
     {
-        dirc = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        dirc = p.position - transform.position + y2;
         if (dirc.magnitude < 0.1f)
         {
             return;
@@ -22,7 +24,7 @@ public class RotateToTarget : MonoBehaviour
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
 
-        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 cursorPos = p.position+ y2;
         transform.position = Vector2.MoveTowards(transform.position, cursorPos, moveSpeed * Time.deltaTime);
     }
 }
